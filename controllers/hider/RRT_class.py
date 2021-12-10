b15d1adde0bb5f04a9c8a5bf94276b59d5595d9a
skywalkaa <49798726+skywalkaa@users.noreply.github.com>
2021-12-10 12:05:47 -0700

    Odometry and drawing to the display works. Map updating is not

--- controllers/hider/RRT_class.py
+++ controllers/hider/RRT_class.py
@@ -9,14 +9,14 @@ class Node:
     def __init__(self,pt,parent = None):
         self.point = pt;
         self.parent = parent;
-        seld.path_from_parent = [];
+        self.path_from_parent = [];
 
 class RRT:
     """
     Class that continually updates a map representaion and runs RRT on it
     """
 
-    def __init__(self, map, config_radius, k, q, bounds, towards_goal_prob=0.1, min_goal_dist = 2):
+    def __init__(self, config_radius, k, q, map_size, towards_goal_prob=0.1, min_goal_dist = 2):
         '''
         RRT on a world initialization
 
@@ -29,17 +29,18 @@ class RRT:
         :param towards_goal_prob: probability that new sampled point will point towards the goal point
         :param min_goal_dist: success criterea for being close to the goal;
         '''
-        self.map = map;
-        self.config_space = []; #configuration space of the robot
-        self.explored_space = [] #map that keeps track of space that has been "explored"
+        self.map = np.zeros((map_size,map_size))
+        self.config_space = np.zeros((map_size,map_size)) #configuration space of the robot
+        self.explored_space = np.zeros((map_size,map_size)) #map that keeps track of space that has been "explored"
         self.path = [] #List of Nodes has not been run
         self.config_radius = config_radius;
         self.k = k; #how many nodes to sample in each run_RRT call
         self.delta_q = q; # max distance between nodes
+        self.state_bounds = [(0,map_size),(0,map_size)]
         self.towards_goal_prob = towards_goal_prob;
+        self.min_goal_dist = min_goal_dist
         # need to initialize RRT list
-        self.root = Node(start_point, parent=None)
-        self.path.append(self.root)
+
 
 
     # def update_RRT(self,from_node, to_node):
@@ -58,65 +59,125 @@ class RRT:
     #         delete_branch(node_to_delete);
     #         run_RRT(from_node, to_node);
 
-    def run_RRT(self, start_point, goal_point):
+    def run_RRT(self, start_point, goal_point, map):
         '''
         runs_RRT on self.config_space
         :paramm start_point: tuple of coordinates to start RRT at
         :param goal_point: tuple of target coordintaes
         :returns: a list of tuples representing a path
         '''
+
         #begin list
         node_list = [];
-        node_list.append(Node(start_point, parent=None))
+        start_point_int = [int(val) for val in start_point]
+        node_list.append(Node(np.array(start_point_int), parent=None))
+
+        self.update_config_space(map)
 
         for i in range(self.k):
+            # print('here in RRT')
+            # print('nodes',[node.point for node in node_list])
             if(random.random()<self.towards_goal_prob):
-                new_point = goal_point
+                new_point = [int(num) for num in np.array(goal_point)]
             else:
-                new_point = get_random_valid_vertex(state_bounds,self.config_space)
+                new_point = np.array(self.get_random_valid_vertex(self.state_bounds,self.config_space))
+            # print('new point', new_point)
+            nearest_node = self.get_nearest_node(node_list, new_point)
+            # print('nearest_node point', nearest_node.point)
 
-            nearest_node = get_nearest_node(node_list, newCorrdinates)
-            path = steer(nearest_node.point, new_point)
+            # print('steering from ', nearest_node.point, ' to ', new_point)
+            path = self.steer(nearest_node.point, new_point)
 
-            if not path_is_valid(path, self.config_space):
+            if not self.path_is_valid(path, self.config_space):
                 continue
 
             # create new node
-            new_node = Node(path[len(path),1:2], Parent = nearest_node)
-            node_list.append(new_node)
+            # print('path',path)
 
+            new_node_point = np.array([int(num) for num in path[len(path)-1,:]])
+            # print('after steer', new_node_point)
+            if not self.point_is_valid(new_node_point,self.config_space): continue
+            new_node = Node(new_node_point, parent = nearest_node)
+            node_list.append(new_node)
+            # print('new node point',new_node.point)
             # check to see if node is close enough to goal
-            if np.linalg.norm(new_node.point-goal_point)<min_goal_dist:
+
+            if np.linalg.norm(np.array(new_node.point)-np.array(goal_point))<self.min_goal_dist:
                 break
 
-        return get_waypoints(node_list)
+        return node_list
 
-    def get_waypoints(nodes):
-        return [node.point for node in nodes]
+    def get_nearest_node(self, nodes, point):
 
-    def steer(from_point, to_point, samples = 10):
+
+        # for i,node in enumerate(nodes):
+        #     distances[i] = np.linalg.norm(node.point-point)
+        distances = [np.linalg.norm(node.point-point) for node in nodes]
+
+        return nodes[np.argmin(distances)]
+
+    def get_waypoints(self,nodes):
+        cur_node = nodes[len(nodes)-1]
+        path = []
+        path.append(cur_node)
+        while cur_node.parent is not None:
+            path.append(cur_node.parent)
+            cur_node = cur_node.parent
+        waypoints = [node.point for node in path]
+
+        return waypoints[::-1]
+
+    def steer(self,from_point, to_point, samples = 10):
         # get vector that points from 'from_point' to 'to_point'
         pointingVec = np.array(to_point)-np.array(from_point)
-        length =
-
+        mag = np.linalg.norm(pointingVec)
+        # print('mag',mag)
         # normalize vector to delta_q if length is greater than delta_q
-        if length > delta_q:
-            pointingVec = delta_q*pointingVec/np.linalg.norm(pointingVec)
-
+        if mag > self.delta_q:
+            pointingVec = self.delta_q*pointingVec/mag
+        # print('pointingVec',pointingVec)
 
         path = np.empty([10, len(pointingVec)], np.float64)
         to_point = from_point+pointingVec
 
         # get n-dimansional path using linspace
+        # print('frpm to point',from_point,to_point)
         for i in range(len(pointingVec)):
             path[:,i] = np.linspace(from_point[i],to_point[i],samples)
 
         return path
 
-    get_random_valid_vertex(bounds,config_space):
-        point = ()
+    def get_random_valid_vertex(self,bounds,config_space):
+        # vertex = []
+        # for i,limit in enumerate(bounds):
+        #     vertex[i] = [random.random()*(limit[1]-limit[0])+limit[0]]\
+        point = np.zeros(len(bounds))
+        for j in range(500):
+            for i,limits in enumerate(bounds):
+                point[i] = int(random.random()*(limits[1]-limits[0])+limits[0])
+            # print('get_random_valid_vertex point',point)
+            if self.point_is_valid(point, config_space): break
 
-    def path_is_valid(points, config_space):
+        if j==499:
+            raise Exception("get_random_valid_vertex did not find a valid point after 500 iterations")
+
+        return point
+
+    def point_is_valid(self, point, config_space):
+        # print('point',point)
+        # need to convert point from meters to pixels
+        # print('config', config_space)
+        x = int(point[0])
+        y = int(point[1])
+        if config_space[x][y] > 0.9:
+            return False
+        if x<self.state_bounds[0][0] or x>self.state_bounds[0][1] or y<self.state_bounds[1][0] or y>self.state_bounds[1][1]:
+            # print('invalid point',x,y)
+            return False
+        # print('valid point',x,y)
+        return True
+
+    def path_is_valid(self, points, config_space):
         '''
         checks if any segment of the path is colliding with the config space
 
@@ -125,9 +186,11 @@ class RRT:
         :returns: true if path is valid
         '''
         for point in points:
-            x,y = point;
-            if config_space[x,y] > 0.9:
+            if not self.point_is_valid(point,config_space):
+                # print('invalid path',points)
                 return False
+        # print('valid path',points)
+        return True
 
     def update_config_space(self,new_map):
         '''
@@ -137,7 +200,33 @@ class RRT:
         :param: new_map: a 2D array contatinng a world representation
         :returns nothing
         '''
+        threshHold = .8 #slightly arbitrary at this point
+        radius = self.config_radius;
+        new_map = np.array(new_map)
 
+        locations_to_update = new_map-self.map;
+
+        rows = locations_to_update.shape[0]
+        cols = locations_to_update.shape[1]
+        for i in range(rows):
+            for j in range(cols):
+                if locations_to_update[i][j] > threshHold:
+                    left = j-radius
+                    right = j+radius
+                    lower = i+radius
+                    upper = i-radius
+
+                    if left<0:
+                        left = 0
+                    if right>cols-1:
+                        right = cols-1
+                    if lower > rows-1:
+                        lower = rows-1
+                    if upper < 0:
+                        upper = 0
+                    for k in range(upper,lower):
+                        for n in range(left,right):
+                            self.config_space[k][n] = 1
 
 
 
--- controllers/hider/hider.py
+++ controllers/hider/hider.py
@@ -5,24 +5,121 @@ import math
 import numpy as np
 from matplotlib import pyplot as plt
 from scipy.signal import convolve2d # Uncomment if you want to use something else for finding the configuration space
+import RRT_class
 
-MAX_SPEED = .5*7.0  # [rad/s]
+MAX_SPEED = .25*7.0  # [rad/s]
 MAX_SPEED_MS = 0.633 # [m/s]
 AXLE_LENGTH = 0.50 # m
 WHEEL_RADIUS = 0.1 # m
-MOTOR_LEFT = 10
-MOTOR_RIGHT = 11
-N_PARTS = 12
+MOTOR_LEFT = 0
+MOTOR_RIGHT = 1
+N_PARTS = 2
 
 LIDAR_ANGLE_BINS = 667
 LIDAR_SENSOR_MAX_RANGE = 2.75 # Meters
 LIDAR_ANGLE_RANGE = math.radians(240)
 
+DISPLAY_LENGTH = 360 #number of pixels on a side of display
+WORLD_XLIMS = (-10,10)
+WORLD_YLIMS = (-10,10)
 
+mode = "explore"
 ##### vvv [Begin] Do Not Modify vvv #####
+
+################FUNCTIONS########################
+def displayMap(map):
+  for disp_x in range(360):
+    for disp_y in range(360):
+        map[disp_x][disp_y] += 0.005
+        if disp_x < 0 or disp_x >= 360 or disp_y < 0 or disp_y >= 360:
+            continue
+
+        if map[disp_x][disp_y] > 0.99:
+            map[disp_x][disp_y] = 0.99
+
+            g = map[disp_x][disp_y]
+            byte = int(g * 255)
+            byte2 = byte * 256
+            byte3 = byte2 * 256
+
+            color = byte + byte2 + byte3
+            #print(hex(color))
+            #if map[disp_x][disp_y] > 1:
+            display.setColor(color)
+            display.drawPixel(disp_x, disp_y)
+
+def clear_display():
+    display.setColor(0x000000)
+    display.fillRectangle(0, 0, DISPLAY_LENGTH, DISPLAY_LENGTH)
+
+def draw_tree(nodes):
+
+
+    for node in nodes:
+        if node.parent is None: continue
+        # print(node.parent.point)
+
+        draw_rect(node.point[0], node.point[1], 3, 0xff0000)
+        display.setColor(0xff00ff)
+        # print('draw tree',node.point[0], node.point[1], node.parent.point[0], node.parent.point[1])
+        display.drawLine(int(node.point[0]), int(node.point[1]), int(node.parent.point[0]), int(node.parent.point[1]))
+
+    # Draw a different color for the path
+    display.setColor(0x00ff10)
+    cur_node = nodes[len(nodes)-1]
+    while cur_node.parent is not None:
+        display.drawLine(int(cur_node.point[0]), int(cur_node.point[1]), int(cur_node.parent.point[0]), int(cur_node.parent.point[1]))
+        cur_node = cur_node.parent
+
+    #start node
+    draw_rect(nodes[0].point[0], nodes[0].point[1], 5, 0x00ff00)
+    #end node
+    draw_rect(nodes[len(nodes)-1].point[0], nodes[len(nodes)-1].point[1], 5, 0xffff00)
+
+def world_to_pixel(point):
+    x,y = point
+    row = int(DISPLAY_LENGTH/(WORLD_XLIMS[1]-WORLD_XLIMS[0])*(x-WORLD_XLIMS[0]))
+    col = int(-DISPLAY_LENGTH/(WORLD_YLIMS[1]-WORLD_YLIMS[0])*(y-WORLD_YLIMS[0])+DISPLAY_LENGTH)
+    return (col,row)
+
+def pixel_to_world(points):
+    coords = []
+    for i,point in enumerate(points):
+        x = point[0]
+        y = point[1]
+        world_x = -x*(WORLD_XLIMS[0]-WORLD_XLIMS[1])/DISPLAY_LENGTH+WORLD_XLIMS[0]
+        world_y = (y-DISPLAY_LENGTH)*(WORLD_YLIMS[0]-WORLD_YLIMS[1])/DISPLAY_LENGTH+WORLD_YLIMS[0]
+        coords.append(np.array([-world_y,-world_x]))
+    return coords
+
+def draw_rect(x, y, radius, color):
+    x = int(x)
+    y = int(y)
+    for disp_x in range(x - radius, x + radius):
+        for disp_y in range(y - radius, y + radius):
+            if disp_x < 0 or disp_x >= 360 or disp_y < 0 or disp_y >= 360:
+                continue
+
+            #byte = int(g * 255)
+            #byte2 = byte * 256
+            #byte3 = byte2 * 256
+
+            #color = byte + byte2 + byte3
+            #print(hex(color))
+
+            #if map[disp_x][disp_y] > 1:
+            display.setColor(color)
+            display.drawPixel(disp_x, disp_y)
+
+def euclid(x, y):
+    return np.sqrt((x[0] - y[0])**2 + (x[1] - y[1]) **2)
+
+
+####################END FUNCTIONS##################
 
 # create the Robot instance.
 robot = Robot()
+print('robot',robot)
 # get the time step of the current world.
 timestep = int(robot.getBasicTimeStep())
 DELTA_TIME = 1/timestep;
@@ -67,6 +164,7 @@ keyboard.enable(timestep)
 # map the 12x12m2 apartment
 display = robot.getDevice("display")
 
+
 # Odometry
 pose_x     = 0
 pose_y     = 0
@@ -80,6 +178,171 @@ lidar_offsets = np.linspace(-LIDAR_ANGLE_RANGE/2., +LIDAR_ANGLE_RANGE/2., LIDAR_
 lidar_offsets = lidar_offsets[83:len(lidar_offsets)-83] # Only keep lidar readings not blocked by robot chassis
 
 map = None
+goal_point = world_to_pixel((7,-2))
+#initialize RRT class
+map = np.zeros([360,360])
+RRT = RRT_class.RRT(config_radius=10,k=200,q=50,map_size=360,min_goal_dist=2)
+
+#do one iteration of run_RRT
+robot.step(timestep)
+pose_y = gps.getValues()[2]
+pose_x = gps.getValues()[0]
+nodes = RRT.run_RRT(world_to_pixel((pose_x,pose_y)),goal_point,map)
+pixel_waypoints = RRT.get_waypoints(nodes)
+waypoints = pixel_to_world(pixel_waypoints)
+draw_tree(nodes)
+
+# cuurent waypoint
+display.drawOval(int(pixel_waypoints[1][0]), int(pixel_waypoints[1][1]), 10, 10)
+
+print('here')
+state = 1;
+goalPoint = world_to_pixel((7,-2)) # make this random?
+while robot.step(timestep) != -1:
+    pose_y = gps.getValues()[2]
+    pose_x = gps.getValues()[0]
+
+    n = compass.getValues()
+    rad = -((math.atan2(n[0], n[2]))-1.5708)
+    pose_theta = rad
+
+    lidar_sensor_readings = lidar.getRangeImage()
+    lidar_sensor_readings = lidar_sensor_readings[83:len(lidar_sensor_readings)-83]
+
+    # lidar stuff
+    for i, rho in enumerate(lidar_sensor_readings):
+        alpha = lidar_offsets[i]
+
+        if rho > LIDAR_SENSOR_MAX_RANGE:
+            continue
+
+        # The Webots coordinate system doesn't match the robot-centric axes we're used to
+        rx = math.cos(alpha)*rho
+        ry = -math.sin(alpha)*rho
+
+        # Convert detection from robot coordinates into world coordinates
+        wx =  math.cos(pose_theta)*rx - math.sin(pose_theta)*ry + pose_x
+        wy =  -(math.sin(pose_theta)*rx + math.cos(pose_theta)*ry) + pose_y
 
 
+        if rho < LIDAR_SENSOR_MAX_RANGE:
+            # Part 1.3: visualize map gray values.
+
+            # You will eventually REPLACE the following 2 lines with a more robust version of the map
+            # with a grayscale drawing containing more levels than just 0 and 1.
+            display.setColor(0xFFFFFF)
+            disp_x = 360-int(wy*30)
+            disp_y = int(wx*30)
+            if (disp_x > 359):
+                disp_x = 359
+
+            if (disp_y > 359):
+                disp_y = 359
+
+            map[disp_x][disp_y] += 0.005
+            if map[disp_x][disp_y] > 0.99:
+                map[disp_x][disp_y] = 0.99
+
+            g = map[disp_x][disp_y]
+            byte = int(g * 255)
+            byte2 = byte * 256
+            byte3 = byte2 * 256
+
+            color = byte + byte2 + byte3
+            #print(hex(color))
+
+
+            #if map[disp_x][disp_y] > 1:
+            display.setColor(color)
+            display.drawPixel(disp_x, disp_y)
+
+    display.setColor(int(0xFF0000))
+    temp = world_to_pixel((pose_x,pose_y))
+    print('temp',temp)
+    x = temp[0]
+    y = temp[1]
+
+    display.drawPixel(x,y)
+
+###################### RRT ######################################
+
+    # print('nodes',nodes)
+    # print('waypoints',RRT.get_waypoints(nodes))
+    # draw_tree(nodes)
+
+    # print('world waypoints',waypoints)
+    print('poses', pose_x, pose_y, waypoints[state])
+    translation_error = euclid([pose_x, pose_y], waypoints[state])
+    if state == len(waypoints) - 1 and translation_error < 0.5:
+        mode = 'done'
+        vL = 0
+        vR = 0
+    elif (translation_error < 0.5):
+        # state+=1;
+        # if state >= len(waypoints):
+        #     state = len(waypoints) - 1
+        pixel_coords = world_to_pixel((pose_x,pose_y))
+        print('pose',(pose_x,pose_y))
+        # print('pixel_coords',pixel_coords)
+        nodes = RRT.run_RRT(pixel_coords,goalPoint,map)
+        pixel_waypoints = RRT.get_waypoints(nodes)
+        waypoints = pixel_to_world(pixel_waypoints)
+        print('goal wayppint',waypoints[state])
+        clear_display();
+        draw_tree(nodes)
+        #raw current node that the robot is travelling to
+        display.drawOval(int(pixel_waypoints[1][0]), int(pixel_waypoints[1][1]), 10, 10)
+        continue
+
+
+    # print('pose',pose_x,pose_y,pose_theta*180/3.14,waypoints[state])
+    rho = 0
+    alpha = -(math.atan2(waypoints[state][1]-pose_y,waypoints[state][0]-pose_x) + pose_theta)
+
+
+
+
+######################CONTROLLER################################
+    dX = 0
+    dTheta = 0
+
+
+    translation_coef = 1;
+    rotation_coeff = 10;
+
+    prop_left = translation_coef*translation_error - (rotation_coeff*alpha*AXLE_LENGTH/2)
+    prop_right = translation_coef*translation_error + (rotation_coeff*alpha*AXLE_LENGTH/2)
+
+    if (prop_left>prop_right):
+        vL = MAX_SPEED;
+        vR = prop_right/prop_left*MAX_SPEED;
+    else:
+        vL = prop_left/prop_right*MAX_SPEED;
+        vR = MAX_SPEED;
+
+    if mode == 'done':
+        vL = 0
+        vR = 0
+    # print('velocities',vL,vR,'props',prop_left,prop_right)
+
+    #odometry
+    # pose_x += (vL+vR)/2/MAX_SPEED*MAX_SPEED_MS*timestep/1000.0*math.cos(pose_theta)
+    # pose_y -= (vL+vR)/2/MAX_SPEED*MAX_SPEED_MS*timestep/1000.0*math.sin(pose_theta)
+    # pose_theta += (vR-vL)/AXLE_LENGTH/MAX_SPEED*MAX_SPEED_MS*timestep/1000.0
+
+    x_dot_robot = WHEEL_RADIUS/2*(vL+vR)
+    w_dot_robot = WHEEL_RADIUS/AXLE_LENGTH*(vR-vL)
+
+    pose_x_dot     = math.cos(pose_theta)*x_dot_robot
+    pose_y_dot     = math.sin(pose_theta)*x_dot_robot
+    pose_theta_dot = w_dot_robot
+
+    pose_x         += pose_x_dot*DELTA_TIME
+    pose_y         += pose_y_dot*DELTA_TIME
+    pose_theta     += pose_theta_dot*DELTA_TIME
+
+    print('translation_error',translation_error, ' bearing error', alpha,' vels ', vL,vR)
+    #set wheel vels
+    robot_parts[MOTOR_LEFT].setVelocity(vL)
+    robot_parts[MOTOR_RIGHT].setVelocity(vR)
 # Functions
