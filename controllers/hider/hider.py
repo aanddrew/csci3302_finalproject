e0c29d75748701140e6017d18442f0b9c1ea0ee7
skywalkaa <49798726+skywalkaa@users.noreply.github.com>
2021-12-13 14:04:38 -0700

    FINAL FINAL FINAL commit

--- controllers/hider/hider.py
+++ controllers/hider/hider.py
@@ -35,32 +35,37 @@ mode = "explorer"
 
 ################FUNCTIONS########################
 def displayMap(map):
-  for disp_x in range(360):
-    for disp_y in range(360):
-        map[disp_x][disp_y] += 0.005
-        if disp_x < 0 or disp_x >= 360 or disp_y < 0 or disp_y >= 360:
-            continue
+    '''
+    Draws 'map' onto the display
+    '''
+    for disp_x in range(360):
+        for disp_y in range(360):
+            map[disp_x][disp_y] += 0.005
+            if disp_x < 0 or disp_x >= 360 or disp_y < 0 or disp_y >= 360:
+                continue
 
-        if map[disp_x][disp_y] > 0.99:
-            map[disp_x][disp_y] = 0.99
+            if map[disp_x][disp_y] > 0.99:
+                map[disp_x][disp_y] = 0.99
 
-            g = map[disp_x][disp_y]
-            byte = int(g * 255)
-            byte2 = byte * 256
-            byte3 = byte2 * 256
+                g = map[disp_x][disp_y]
+                byte = int(g * 255)
+                byte2 = byte * 256
+                byte3 = byte2 * 256
 
-            color = byte + byte2 + byte3
-            #print(hex(color))
-            #if map[disp_x][disp_y] > 1:
-            display.setColor(color)
-            display.drawPixel(disp_x, disp_y)
+                color = byte + byte2 + byte3
+                #print(hex(color))
+                #if map[disp_x][disp_y] > 1:
+                display.setColor(color)
+                display.drawPixel(disp_x, disp_y)
 
 def clear_display():
     display.setColor(0x000000)
     display.fillRectangle(0, 0, DISPLAY_LENGTH, DISPLAY_LENGTH)
 
 def draw_tree(nodes):
-
+    '''
+    Draws the RRT onto the display
+    '''
 
     for node in nodes:
         if node.parent is None: continue
@@ -84,12 +89,18 @@ def draw_tree(nodes):
     draw_rect(nodes[len(nodes)-1].point[0], nodes[len(nodes)-1].point[1], 5, 0xffff00)
 
 def world_to_pixel(point):
+    '''
+    From world coordinates to pixel/display coordinates
+    '''
     x,y = point
     row = int(DISPLAY_LENGTH/(WORLD_XLIMS[1]-WORLD_XLIMS[0])*(x-WORLD_XLIMS[0]))
     col = int(-DISPLAY_LENGTH/(WORLD_YLIMS[1]-WORLD_YLIMS[0])*(y-WORLD_YLIMS[0])+DISPLAY_LENGTH)
     return (col,row)
 
 def pixel_to_world(points):
+    '''
+    From pixel/display coordinates to world coordinates
+    '''
     coords = []
     for i,point in enumerate(points):
         x = point[0]
@@ -100,6 +111,9 @@ def pixel_to_world(points):
     return coords
 
 def draw_rect(x, y, radius, color):
+    '''
+    Draws a rectangle with center, radius, and color
+    '''
     x = int(x)
     y = int(y)
     for disp_x in range(x - radius, x + radius):
@@ -122,44 +136,12 @@ def euclid(x, y):
     return np.sqrt((x[0] - y[0])**2 + (x[1] - y[1]) **2)
 
 def get_new_goal_point():
+    '''
+    Gets a random point within the world bounds
+    '''
 
-    # x = random.random()*(WORLD_XLIMS[1]-WORLD_XLIMS[0])+WORLD_XLIMS[0]
-    # y = random.random()*(WORLD_YLIMS[1]-WORLD_YLIMS[0])+WORLD_YLIMS[0]
     point = RRT.get_random_valid_vertex([(0,DISPLAY_LENGTH),(0,DISPLAY_LENGTH)],RRT.get_config_space())
     return point
-
-def get_closest_valid_point(config_space):
-    locs = world_to_pixel((pose_x,pose_y))
-    col = locs[0]
-    row = locs[1]
-
-    cols = config_space.shape[1]
-    rows = config_space.shape[0]
-
-    radius = 0
-    while True:
-        radius+=1
-
-        left = col-radius
-        right = col+radius
-
-        upper = row-radius
-        lower = row+radius
-
-        if left<0:
-            left = 0
-        if right>cols-1:
-            right = cols-1
-        if lower > rows-1:
-            lower = rows-1
-        if upper < 0:
-            upper = 0
-
-        if config_space[col][upper]<0.1: return pixel_to_world([col,upper])
-        if config_space[col][lower]<0.1: return pixel_to_world([col,lower])
-        if config_space[left][row]<0.1: return pixel_to_world([left,row])
-        if config_space[right][row]<0.1: return pixel_to_world([right,row])
-
 
 ####################END FUNCTIONS##################
 
@@ -187,11 +169,7 @@ for i in range(N_PARTS):
 # The Tiago robot has a couple more sensors than the e-Puck
 # Some of them are mentioned below. We will use its LiDAR for Lab 5
 
-# range = robot.getDevice('range-finder')
-# range.enable(timestep)
-# camera = robot.getDevice('camera')
-# camera.enable(timestep)
-# camera.recognitionEnable(timestep)
+
 lidar = robot.getDevice('Hokuyo URG-04LX-UG01')
 lidar.enable(timestep)
 lidar.enablePointCloud()
@@ -225,11 +203,13 @@ lidar_offsets = lidar_offsets[83:len(lidar_offsets)-83] # Only keep lidar readin
 
 map = None
 #initialize RRT class
-map = np.zeros([360,360])
+map = np.zeros([DISPLAY_LENGTH,DISPLAY_LENGTH])
 RRT = RRT_class.RRT(config_radius=CONFIG_RADIUS,k=RRT_ITERATIONS,q=DELTA_Q,map_size=360,min_goal_dist=4, towards_goal_prob=TOWARDS_GOAL)
+
+#get a random goal point
 goal_point = get_new_goal_point()
 
-#do one iteration of run_RRT
+#do one iteration of run_RRT befor while loop
 robot.step(timestep)
 pose_y = gps.getValues()[2]
 pose_x = gps.getValues()[0]
@@ -239,17 +219,19 @@ waypoints = pixel_to_world(pixel_waypoints)
 draw_tree(nodes)
 
 # cuurent waypoint
-display.drawOval(int(pixel_waypoints[1][0]), int(pixel_waypoints[1][1]), 10, 10)
+# display.drawOval(int(pixel_waypoints[1][0]), int(pixel_waypoints[1][1]), 10, 10)
 
 
 state = 1;
 goalPoint = world_to_pixel((7,-2)) # make this random?
 loop_count = -1;
 flag = False
+print_flag = False
+print('mode: ',mode)
 while robot.step(timestep) != -1:
-
     loop_count+=1
 
+    #get location values from GPS
     pose_y = gps.getValues()[2]
     pose_x = gps.getValues()[0]
 
@@ -257,6 +239,7 @@ while robot.step(timestep) != -1:
     rad = -((math.atan2(n[0], n[2]))-1.5708)
     pose_theta = rad
 
+    #get lidar values
     lidar_sensor_readings = lidar.getRangeImage()
     lidar_sensor_readings = lidar_sensor_readings[83:len(lidar_sensor_readings)-83]
 
@@ -316,7 +299,7 @@ while robot.step(timestep) != -1:
 
     display.drawPixel(x,y)
 
-    # update config space
+    # update config space every 100 iterations of loop
     if loop_count%100==0:
         RRT.update_config_space(map)
         displayMap(RRT.get_config_space())
@@ -325,65 +308,65 @@ while robot.step(timestep) != -1:
 
 ###################### RRT ######################################
     if mode == 'explorer' and robot.getTime()<HIDING_TIME:
-        # print('nodes',nodes)
-        # print('waypoints',RRT.get_waypoints(nodes))
-        # draw_tree(nodes)
 
-        # print('world waypoints',waypoints)
-        # print('poses', pose_x, pose_y, waypoints[state])
         translation_error = euclid([pose_x, pose_y], waypoints[state])
+
+        # If reached current goal_point: rerun RRT
         if state == len(waypoints) - 1 and translation_error < 0.15:
             state = 1
-            print('New goal point line 334',goal_point)
+            # print('New goal point line 334',goal_point)
             goal_point = get_new_goal_point()
             pixel_coords = world_to_pixel((pose_x,pose_y))
             nodes = RRT.run_RRT(pixel_coords,goalPoint,map)
+            if len(nodes)<2:
+                state = 0;
             pixel_waypoints = RRT.get_waypoints(nodes)
             waypoints = pixel_to_world(pixel_waypoints)
+
+            #draw on display
             clear_display();
             draw_tree(nodes)
             displayMap(RRT.get_config_space())
             #raw current node that the robot is travelling to
-            display.drawOval(int(pixel_waypoints[1][0]), int(pixel_waypoints[1][1]), 10, 10)
+            # display.drawOval(int(pixel_waypoints[1][0]), int(pixel_waypoints[1][1]), 10, 10)
 
-        # Is the RRT needs to be rebuilt
+        # move on to next waypoint
         elif (translation_error < 0.15):
             state+=1
 
+        # check for collisions along path with updated config map
         if RRT.check_collisions_along_path(nodes) or not RRT.point_is_valid(goal_point,RRT.get_config_space()):
 
+            #specifically check goal point is valid. RRT will may not work if goal_point is invalid
             if not RRT.point_is_valid(goal_point,RRT.get_config_space()):
-                print('New goal point line 352',goal_point)
+                # print('New goal point line 352',goal_point)
                 goal_point = get_new_goal_point();
 
             state = 1
             pixel_coords = world_to_pixel((pose_x,pose_y))
             nodes = RRT.run_RRT(pixel_coords,goalPoint,map)
+            if len(nodes)<2:
+                state = 0;
             pixel_waypoints = RRT.get_waypoints(nodes)
             waypoints = pixel_to_world(pixel_waypoints)
             clear_display();
             draw_tree(nodes)
             displayMap(RRT.get_config_space())
-            #raw current node that the robot is travelling to
-            display.drawOval(int(pixel_waypoints[1][0]), int(pixel_waypoints[1][1]), 10, 10)
             continue
 
-
-        # print('pose',pose_x,pose_y,pose_theta*180/3.14,waypoints[state])
-
+        # if the robot happens to be in config space, need to get it out independantly of RRT.
+        # set mode into 'backup' which gets the robot out of config space.
         if not RRT.point_is_valid(world_to_pixel((pose_x,pose_y)),RRT.get_config_space()):
-            print('point is not valid line 370')
             mode = 'backup'
+            print('mode: ',mode)
             if not flag:
                 backup_start_time = robot.getTime()
                 flag = True
-            # if lidar_sensor_readings[0]>1:
-            #     alpha = -1.56
-            # else:
-            #     alpha = 1.56
-            # alpha = -(math.atan2(temp_goal[1]-pose_y,temp_goal[0]-pose_x) + pose_theta)
+
+    # done hiding after HIDING_TIME seconds
     elif robot.getTime()>HIDING_TIME:
         mode = 'done'
+        print('hider is done exploring and is now hiding.')
 
 
     rho = 0
@@ -410,34 +393,50 @@ while robot.step(timestep) != -1:
         vL = prop_left/prop_right*MAX_SPEED;
         vR = MAX_SPEED;
 
+    # The robot would steer too wide of points if not for these statements
     if alpha>.25:
         vL = -MAX_SPEED/2
         vR = MAX_SPEED/2;
     elif alpha<-.25:
         vL = MAX_SPEED/2
         vR = -MAX_SPEED/2;
-    # else:
-    #     vL = MAX_SPEED/2
-    #     vR = MAX_SPEED/2;
+
+    # this is what gete robot out of config space. Essentially, it just tries to
+    # keep moving foward, then spin after half a second. Then it repeats.
     if mode == 'backup':
-        print('mode = backup', )
+        # print('mode = backup', )
         vL = MAX_SPEED/2
         vR = MAX_SPEED/2;
         if robot.getTime()-backup_start_time>0.5 and robot.getTime()-backup_start_time<2:
-            print('turning')
+            # print('turning')
             vL = MAX_SPEED/2
             vR = -MAX_SPEED/2;
         elif robot.getTime()-backup_start_time>2:
             backup_start_time = robot.getTime()
+
+        # if the robot is out of config space, rerun RRT.
         if RRT.point_is_valid(world_to_pixel((pose_x,pose_y)),RRT.get_config_space()):
+            state = 1
             mode = 'explorer'
+            print('mode: ',mode)
+            pixel_coords = world_to_pixel((pose_x,pose_y))
+            nodes = RRT.run_RRT(pixel_coords,goalPoint,map)
+            if len(nodes)<2:
+                state = 0;
+            pixel_waypoints = RRT.get_waypoints(nodes)
+            waypoints = pixel_to_world(pixel_waypoints)
+            clear_display();
+            draw_tree(nodes)
+            displayMap(RRT.get_config_space())
+            #raw current node that the robot is travelling to
+            # display.drawOval(int(pixel_waypoints[1][0]), int(pixel_waypoints[1][1]), 10, 10)
+            # mode = 'explorer'
             flag = False
 
     if mode == 'done':
-        print('hider is done exploring and is now hiding.')
         vL = 0
         vR = 0
-    print(mode)
+    # print(mode)
     # print('velocities',vL,vR,'props',prop_left,prop_right)
 
     #odometry
